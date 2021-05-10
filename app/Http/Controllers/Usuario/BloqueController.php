553407c81acb5d0a43e6150277dcd84f2760a21f
Sistemas Cerízeon <sistemascerizeon@gmail.com>
2021-05-10 00:36:42 -0400

    permisos 777

--- app/Http/Controllers/Usuario/BloqueController.php
+++ app/Http/Controllers/Usuario/BloqueController.php
@@ -12,87 +12,6 @@ use File;
 
 class BloqueController extends Controller
 {
-    public function store(Request $request)
-    {
-        $cart = json_decode($request['cart']);
-        $matriz=Grip::find($request['matriz_id']);
-        $mytemp=json_decode($matriz->matriz);
-        $originalimg = \Image::make($request->file('img'));
-        $imaor=$request->file('img');
-        $pathoriginal = $imaor->store('public/Grids');
-        $pathoriginal = str_replace('public/', '', $pathoriginal);
-        $aumentox=600/$request['column'];
-        $aumentoy=600/$request['fila'];
-        $origenx=300;
-        $mybloques= collect();
-        $characters2 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ*-%$&';
-        $characters2Length = strlen($characters2);
-        $randomString2 = '';
-            for ($i = 0; $i < 15; $i++) {
-                $randomString2 .= $characters2[rand(0, $characters2Length - 1)];
-        }
-        foreach ($cart as $bloque) {  
-            $newbloque = Bloque::create([
-                'column' => $bloque->columna,
-                'fila' =>   $bloque->fila,
-                'codigo' => $randomString2,
-                'matriz_id' => $request['matriz_id'],
-            ]);
-
-            $mybloques->push($newbloque);
-        }
-
-
-        for ($i=0; $i < $request['fila']; $i++) {
-
-            $origeny=0;
-            for ($j=0; $j < 2; $j++) { 
-                $bloquecaptura=null;
-                $bloquecaptura=Bloque::where('codigo',$randomString2)->where('fila',$i)->where('column',$j)->first();
-
-                $img = \Image::make($request->file('img'));
-                $img->resize(600,600);
-                $temp=null;
-                $temp = $img->crop(300,300,$origenx,$origeny);
-                $temp->resize(25,25);
-                $path = public_path().'/storage/Gridsmin/';
-
-                if (!file_exists($path)) {
-                    mkdir($path, 666, true);
-                }
-                $temp->save($path.$bloquecaptura->id.str_replace(' ','_',$request->file('img')->getClientOriginalName()));
-                $bloquecaptura->img= $pathoriginal;
-                $bloquecaptura->fragmento = 'Gridsmin/'.$bloquecaptura->id.str_replace(' ','_',$request->file('img')->getClientOriginalName());
-                $bloquecaptura->save();
-                $origeny=$origeny+$aumentoy;
-
-            }
-
-            $origenx = $origenx - $aumentox;
-
-        }
-
-        //relacionar-en-la-matriz
-        $vincules = Bloque::where('codigo',$mybloques[0]->codigo)->get();
-
-
-
-
-        foreach ($vincules as $element) {
-
-                $mytemp = json_decode($matriz->matriz);
-
-                $mytemp[$element->fila][$element->column]->src=$element->fragmento;
-
-                $mytemp = json_encode($mytemp);
-                $matriz->matriz = $mytemp;
-                $matriz->save();
-        }
-
-        $ruta="/grid/".$matriz->nombreURL;
-
-        return $ruta;
-    }
     public function store2(Request $request)
     {
         $cart = json_decode($request['cart']);
@@ -134,7 +53,7 @@ class BloqueController extends Controller
                     $temp->resize(25,25);
                     $path = public_path().'/storage/Gridsmin/';
                     if (!file_exists($path)) {
-                        mkdir($path, 666, true);
+                        mkdir($path, 0777, true);
                     }
                     $temp->save($path.$randomString.str_replace(' ','_',$request->file('img')->getClientOriginalName()));
                     $myImg->push([
